cd668038114e45e376b33585501b097e4937b3de
joony <joony@kakaopage.com>
2020-07-21 10:19:01 +0900

    todos

--- config/admin.py
+++ config/admin.py
@@ -1,23 +1,12 @@
 from django.contrib import admin
 
-from mydjango.codes.models import CodeGroup, Code
+from mydjango.todos.models import Todo
 
 
-@admin.register(CodeGroup)
-class CodeGroupAdmin(admin.ModelAdmin):
+@admin.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
     fields = (
-        "code_group",
-        "name",
-    )
-
-
-@admin.register(Code)
-class CodeAdmin(admin.ModelAdmin):
-    fields = (
-        "code_group",
-        "code",
-        "name",
-        "available",
-        "display_order",
-        "authority",
+        "id",
+        "text",
+        "checked",
     )
---
+++ mydjango/todos/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TodosAppConfig(AppConfig):
+    name = "mydjango.todos"
+    verbose_name = "Todos"
